5f399a97816401cec276081263b740caf1b75c72
Filipe Husak <69702727+flp04@users.noreply.github.com>
2024-09-02 08:07:42 -0300

    realizada solucoes do desafio

--- Program.cs
+++ Program.cs
@@ -8,18 +8,27 @@ List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+// Pessoa p3 = new Pessoa(nome: "Hóspede 3"); // testando adição de mais um hóspede
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+// hospedes.Add(p3);
 
 // Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+try
+{
+  // Cria uma nova reserva, passando a suíte e os hóspedes
+  Reserva reserva = new Reserva(diasReservados: 10);
+  reserva.CadastrarSuite(suite);
+  reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+  // Exibe a quantidade de hóspedes e o valor da diária
+  Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+  Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria().ToString("C")}");
+}
+catch (Exception ex)
+{
+  Console.WriteLine(ex.Message);
+}
